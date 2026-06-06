7b35c14fd9926220147e3b7d4965af5f9d183bcf
NehuenZ12 <aldonehuen123@gmail.com>
2026-06-06 01:31:45 -0300

    MiCoinSuite

---
+++ TPTestingMonedas/test/test/MiCoinSuite.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author Nehuen
+ */
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+
+    ConversorMonedaTest.class,
+    CotizacionTest.class,
+    DolaritosTest.class
+
+})
+
+public class MiCoinSuite {
+
+}
